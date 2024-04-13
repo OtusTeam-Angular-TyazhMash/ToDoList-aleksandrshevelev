a89d4f0c5b5ac31c6c849b7fb05b792e64b795c8
ShevelevAleksandr <cfy2510@yandex.ru>
2024-04-13 15:35:37 +0400

    Домашнее задание 2

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDoListItem } from 'src/app/models/to-do-list-models';
 
 @Component({
     selector: 'app-to-do-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
     styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent {
+    toDoListItems: Array<ToDoListItem> = [
+        { id: 0, text: "Complete task 1" },
+        { id: 1, text: "Complete task 2" },
+        { id: 2, text: "Complete task 3" },
+        { id: 3, text: "Complete task 4" },
+        { id: 4, text: "Complete task 5" },
+    ];
+    toDoInputValue: string = "";
 
+    addToDoListItem(): void {
+        const maxItemId: number = Math.max(...this.toDoListItems.map(item => item.id), -1);
+        this.toDoListItems.push({
+            id: maxItemId + 1,
+            text: this.toDoInputValue
+        });
+        this.toDoInputValue = "";
+    }
+
+    deleteToDoListItem(itemId: ToDoListItem["id"]): void {
+        const itemIndex: number = this.toDoListItems.findIndex(item => item.id === itemId);
+        if (itemIndex > -1)
+            this.toDoListItems.splice(itemIndex, 1);
+    }
 }
