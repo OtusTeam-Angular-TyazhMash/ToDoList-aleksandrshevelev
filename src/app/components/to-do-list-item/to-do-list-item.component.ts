a55695c750075f3eb853abb9ee75b37446ccfb36
Шевелев Александр Витальевич <91747@tyazhmash.com>
2024-04-17 14:26:28 +0400

    Домашнее задание 3

--- src/app/components/to-do-list-item/to-do-list-item.component.ts
+++ src/app/components/to-do-list-item/to-do-list-item.component.ts
@@ -4,7 +4,7 @@ import { ToDoListItem } from 'src/app/models/to-do-list-models';
 @Component({
     selector: 'app-to-do-list-item',
     templateUrl: './to-do-list-item.component.html',
-    styleUrls: ['./to-do-list-item.component.scss']
+    styleUrls: ['./to-do-list-item.component.scss'],
 })
 export class ToDoListItemComponent {
     @Input({ required: true }) toDoListItem!: ToDoListItem;
