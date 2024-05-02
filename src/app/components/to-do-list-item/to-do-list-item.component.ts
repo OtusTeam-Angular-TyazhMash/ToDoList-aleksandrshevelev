7ee36829281178fd1422e318e4d72201f63997df
Шевелев Александр Витальевич <91747@tyazhmash.com>
2024-05-02 16:38:21 +0400

    Домашнее задание 5

--- src/app/components/to-do-list-item/to-do-list-item.component.ts
+++ src/app/components/to-do-list-item/to-do-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ToDoListItem } from 'src/app/models/to-do-list-models';
 
 @Component({
@@ -6,11 +6,23 @@ import { ToDoListItem } from 'src/app/models/to-do-list-models';
     templateUrl: './to-do-list-item.component.html',
     styleUrls: ['./to-do-list-item.component.scss'],
 })
-export class ToDoListItemComponent {
+export class ToDoListItemComponent implements OnInit {
     @Input({ required: true }) toDoListItem!: ToDoListItem;
+    @Input() isEditMode = false;
     @Output() deleteItemEvent = new EventEmitter<ToDoListItem["id"]>();
+    @Output() editItemEvent = new EventEmitter<ToDoListItem>();
+    itemTitle!: ToDoListItem["text"];
 
-    emitItemDeletion(itemId: ToDoListItem["id"]): void {
-        this.deleteItemEvent.emit(itemId);
+    ngOnInit(): void {
+        this.itemTitle = this.toDoListItem ? this.toDoListItem.text : "";
+    }
+
+    emitItemDeletion(): void {
+        this.deleteItemEvent.emit(this.toDoListItem.id);
+    }
+
+    emitItemEditing(): void {
+        this.toDoListItem.text = this.itemTitle;
+        this.editItemEvent.emit(this.toDoListItem);
     }
 }
